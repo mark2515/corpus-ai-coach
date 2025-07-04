cb5970997a9eb2e5f1ecadb073b22d1915b2cb71
mark2515 <hekunlong2515@gmail.com>
2025-07-03 17:40:57 -0700

    Add a concurrently run script

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,17 @@
 import express from "express";
+import cors from "cors";
+import users from "./data/users";
 
 const app = express();
+app.use(cors());
 const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World from TypeScript!");
+});
+
+app.get('/api/users', (req, res) => {
+    res.json(users);
 });
 
 app.listen(port, () => {
