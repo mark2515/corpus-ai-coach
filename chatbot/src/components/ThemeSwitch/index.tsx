ba6479d6627c50a301dcc0b47acf1f444c7ba5ea
mark2515 <hekunlong2515@gmail.com>
2025-09-22 15:30:32 -0700

    Clean up ThemeSwitch

--- chatbot/src/components/ThemeSwitch/index.tsx
+++ chatbot/src/components/ThemeSwitch/index.tsx
@@ -1,23 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { ActionIcon } from "@mantine/core";
 import { IconSun, IconMoon } from "@tabler/icons-react";
 import { useMantineColorScheme } from "@mantine/core";
 
 export const ThemeSwitch = () => {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
-  const [icon, setIcon] = useState(<IconSun />);
-  useEffect(() => {
-    setIcon(colorScheme === "dark" ? <IconMoon /> : <IconSun />);
-  }, [colorScheme]);
+  
   return (
     <ActionIcon
       variant="subtle"
       size="xs"
-      onClick={() =>
-        toggleColorScheme(colorScheme === "light" ? "dark" : "light")
-      }
+      onClick={() => toggleColorScheme()}
     >
-      {icon}
+      {colorScheme === "dark" ? <IconMoon /> : <IconSun />}
     </ActionIcon>
   );
 };
